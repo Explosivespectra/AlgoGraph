8a7b2bfecfec62bd4b7799a09c239af4c5b8fc0c
Explosivespectra <playonly88@gmail.com>
2021-02-05 21:28:06 -0500

    Added additional sorts accessible via dropdown from appbar; refactoring done to accomodate new sorts; additional work needed for visualization and potential additional themes for more practice with typography/palettes/themes and improved clarity on sorting behavior for users

--- src/components/BodyContent.js
+++ src/components/BodyContent.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { BarGraph } from './d3/BarGraph';
-import { Button, IconButton, AppBar, Toolbar, Dialog, DialogTitle, DialogActions, DialogContent, Slider, Typography, Grid } from '@material-ui/core'
+import { Button, IconButton, AppBar, Toolbar, Dialog, DialogTitle, DialogActions, DialogContent, Menu, MenuItem, Slider, Typography, Grid } from '@material-ui/core'
 import TuneIcon from '@material-ui/icons/Tune';
 import BarChartIcon from '@material-ui/icons/BarChart';
 import ColorLensIcon from '@material-ui/icons/ColorLens';
@@ -11,15 +11,101 @@ const useStyles = makeStyles(theme => ({
 
 }));
 
-const insertionStep = (arr, ind) => {
-  let x = ind;
-  while ( x > 0 && arr[x] < arr[x - 1]) {
-    let copy = arr[x - 1];
-    arr[x - 1] = arr[x];
-    arr[x] = copy;
-    x--;
+const randomData = (range, count) => {
+  let len = count;
+  return [...Array(len).keys()].map((num) => {
+    return Math.floor(Math.random() * (range[1] - range[0])) + range[0];
+  });
+}
+
+const sortParam = {
+  insertion: {
+    pos: 1,
+    ind: 1,
+    completed: false,
+  },
+  selection: {
+    pos: 1,
+    posSmall: 0,
+    ind: 0,
+    completed: false,
+  },
+  bubble: {
+    pos: 0,
+    changes: false,
+    completed: false,
   }
-  return arr;
+}
+
+const insertionStep = (arr, param) => {
+  if ( param.pos > 0 && arr[param.pos] < arr[param.pos - 1]) {
+    let copy = arr[param.pos - 1];
+    arr[param.pos - 1] = arr[param.pos];
+    arr[param.pos] = copy;
+    param.pos--;
+  }
+  else {
+    param.ind = param.ind + 1;
+    param.pos = param.ind;
+  }
+  if (param.ind >= arr.length) {
+    param.completed = true;
+  }
+  return {arr, param};
+}
+
+const selectionStep = (arr, param) => {
+  console.log(param);
+  if (param.pos < arr.length) {  
+    if ( arr[param.pos] < arr[param.posSmall]) {
+      param.posSmall = param.pos;
+    }
+    param.pos = param.pos + 1;
+  }
+  else {
+    let copy = arr[param.ind];
+    arr[param.ind] = arr[param.posSmall];
+    arr[param.posSmall] = copy;
+    param.ind = param.ind + 1;
+    param.posSmall = param.ind;
+    param.pos = param.ind + 1;
+  }
+  if ( param.ind >= arr.length - 1) {
+    param.completed = true;
+  }
+  return {arr, param};
+}
+
+const bubbleStep = (arr, param) => {
+  if (param.pos < arr.length - 1) {
+    if (arr[param.pos] > arr[param.pos + 1]) {
+      param.changes = true;
+      let copy = arr[param.pos + 1];
+      arr[param.pos + 1] = arr[param.pos];
+      arr[param.pos] = copy;
+    }
+    param.pos += 1;
+  }
+  else {
+    if (param.changes) {
+      param.pos = 0;
+      param.changes = false;
+    }
+    else {
+      param.completed = true;
+    }
+  }
+  return {arr, param};
+}
+
+const sortStep = {
+  insertion: (arr, param) => {return insertionStep(arr, param)},
+  selection: (arr, param) => {return selectionStep(arr, param)},
+  bubble: (arr, param) => {return bubbleStep(arr, param)},
+}
+
+const callSortStep = (arr, param, sortType) => {
+  return (sortStep[sortType])(arr, param);
 }
 
 const NumberDialog = ({ defRange, defCount, isOpen, handleClose, handleConfirm }) => {
@@ -38,9 +124,9 @@ const NumberDialog = ({ defRange, defCount, isOpen, handleClose, handleConfirm }
 			<DialogTitle>Number Generation Settings</DialogTitle>
 			<DialogContent>
 				<Typography gutterBottom>Range</Typography>
-				<Slider defaultValue={defRange} value={range} min={0} max={1000} onChange={(event, newValue) => { setRange(newValue) }} valueLabelDisplay="auto" />
+				<Slider defaultValue={defRange} value={range} min={0} max={50} onChange={(event, newValue) => { setRange(newValue) }} valueLabelDisplay="auto" />
 				<Typography gutterBottom>Count</Typography>
-				<Slider defaultValue={defCount} value={count} min={2} max={1000} onChange={(event, newValue) => { setCount(newValue) }} valueLabelDisplay="auto" />
+				<Slider defaultValue={defCount} value={count} min={2} max={40} onChange={(event, newValue) => { setCount(newValue) }} valueLabelDisplay="auto" />
 			</DialogContent>
 			<DialogActions>
 				<Button onClick={close}>Cancel</Button>
@@ -69,20 +155,17 @@ const BodyContent = ({styles, setStyle}) => {
 	const [range, setRange] = useState([0, 17]);
 	const [count, setCount] = useState(15)
 	const [numDialogOpen, setNumDialog] = useState(false);
-	const [styleDialogOpen, setStyleDialog] = useState(false);
+  const [styleDialogOpen, setStyleDialog] = useState(false);
+  const [sortMenuAnchor, setSortMenu] = useState(null);
+  const [sortType, setSortType] = useState("insertion");
+  const [sortIndex, setSortIndex] = useState(0);
   const [tick, setTick] = useState(0);
 
-  const ind = useRef(1);
+  const originalData = useRef([...dummyData]);
+  const sortInfo = useRef({...(sortParam.insertion)});
 
 	const theme = useTheme();
 	console.log(theme);
-
-	const randomData = () => {
-		let len = count;
-		return [...Array(len).keys()].map((num) => {
-			return Math.floor(Math.random() * (range[1] - range[0])) + range[0];
-		});
-	}
 
 	const setNumbers = (newRange, newCount) => {
 		setNumDialog(false);
@@ -91,19 +174,19 @@ const BodyContent = ({styles, setStyle}) => {
 	}
 
   const startInterval = () => {
-    ind.current = 1;
-    setTick(1000);
+    setTick(900);
   }
 
   const resetInterval = () => {
     setTick(0);
-    ind.current = 1;
+    sortInfo.current = {...(sortParam[sortType])};
   }
 
   useInterval(() => {
-    if (ind.current < dummyData.length) {
-      setDummy(insertionStep([...dummyData],ind.current));
-      ind.current = ind.current + 1;
+    if (!sortInfo.current.completed) {
+      let result = callSortStep([...dummyData],sortInfo.current,sortType);
+      setDummy(result.arr);
+      sortInfo.current = result.param;
     }
     else {
       setTick(0);
@@ -114,9 +197,22 @@ const BodyContent = ({styles, setStyle}) => {
 		<>
 			<AppBar position="fixed">
 				<Toolbar>
-					<IconButton color="inherit">
+					<IconButton color="inherit" onClick={(event) => { setSortMenu(event.currentTarget) }}>
 						<BarChartIcon />
 					</IconButton>
+          <Menu 
+            keepmounted open={Boolean(sortMenuAnchor)} 
+            anchorEl={sortMenuAnchor}
+            onClose={() => { setSortMenu(null) }}
+          >
+            {Object.keys(sortParam).map((sort, index) => {
+              return (
+                <MenuItem key={sort} selected={index === sortIndex} onClick={() => { resetInterval(); setSortType(sort); setSortIndex(index); setDummy(originalData.current); sortInfo.current = {...(sortParam[sort])} }}>
+                  {sort}
+                </MenuItem>
+              )
+            })}
+          </Menu>
 					<IconButton color="inherit" onClick={() => { setNumDialog(true) }}>
 						<TuneIcon />
 					</IconButton>
@@ -130,13 +226,13 @@ const BodyContent = ({styles, setStyle}) => {
 				<Grid item>
 					<Grid container direction="column" justify="center" alignItems="center" spacing={1}>
 						<Grid item>
-							<Button onClick={() => { resetInterval(); setDummy(randomData) }} variant="contained" color="primary">Randomize</Button>
+              <Button onClick={() => { resetInterval(); let newData = randomData(range, count); setDummy(newData); originalData.current = [...newData] }} variant="contained" color="primary">Randomize</Button>
 						</Grid>
 						<Grid item>
 							<Button onClick={() => { resetInterval(); let newData = [...dummyData]; setDummy(newData.sort((a, b) => a - b)) }} variant="contained" color="primary">Sort</Button>
 						</Grid>
 						<Grid item>
-							<Button onClick={() => {(tick === 0) ? (setTick(1000)) : (setTick(0))}} variant="contained" color="primary">Begin Sort</Button>
+							<Button onClick={() => {(tick === 0) ? (startInterval()) : (setTick(0))}} variant="contained" color="primary">Begin Sort</Button>
 						</Grid>		
 					</Grid>
 				</Grid>
